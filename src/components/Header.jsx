6353c1089624639601cbc28ecb79966d614f10a5
ADITYA PAREEK <80575917+Aditya01237@users.noreply.github.com>
2025-05-31 01:12:36 +0530

    added some external svg to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="bg-gray-50 shadow-md p-4 pt-4">
       <div className="flex justify-around items-center">
         {/* Left Section: Logo and Location */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
           <img
             className="w-16 h-16 md:w-24 md:h-24"
             src="../../assets/logo/logo2.png"
@@ -17,6 +17,7 @@ const Header = () => {
           <p className="cursor-pointer text-sm md:text-base">
             <b>Bangalore</b>, Karnataka
           </p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin-icon lucide-map-pin"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><circle cx="12" cy="10" r="3"/></svg>
         </div>
 
         {/* Hamburger Menu Button - Visible only on mobile */}
@@ -29,10 +30,22 @@ const Header = () => {
 
         {/* Nav Items (Desktop only) */}
         <ul className="hidden md:flex justify-between items-center gap-10 text-lg font-semibold">
-          <li className="cursor-pointer">offers</li>
-          <li className="cursor-pointer">Help</li>
-          <li className="cursor-pointer">👤Aditya</li>
-          <li className="cursor-pointer">🛒Cart</li>
+          <div className="flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-percent-icon lucide-badge-percent"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/><path d="m15 9-6 6"/><path d="M9 9h.01"/><path d="M15 15h.01"/></svg>
+            <li className="cursor-pointer">offers</li>
+          </div>
+          <div className="flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-info-icon lucide-badge-info"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/><line x1="12" x2="12" y1="16" y2="12"/><line x1="12" x2="12.01" y1="8" y2="8"/></svg>
+            <li className="cursor-pointer">Help</li>
+          </div>
+          <div className="flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+            <li className="cursor-pointer">Aditya</li>
+          </div>
+          <div className="flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart-icon lucide-shopping-cart"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg>
+            <li className="cursor-pointer">Cart</li>
+          </div>
         </ul>
       </div>
 
@@ -41,8 +54,8 @@ const Header = () => {
         <ul className="md:hidden mt-4 flex flex-col gap-4 text-base font-semibold">
           <li className="cursor-pointer">offers</li>
           <li className="cursor-pointer">Help</li>
-          <li className="cursor-pointer">👤Aditya</li>
-          <li className="cursor-pointer">🛒Cart</li>
+          <li className="cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>Aditya</li>
+          <li className="cursor-pointer">Cart</li>
         </ul>
       )}
     </div>
--- src/components/RestCards.jsx
+++ src/components/RestCards.jsx
@@ -28,9 +28,12 @@ const RestCards = ({ data }) => {
       </div>
       <div className="p-1 w-64">
         <h2 className="mt-4 font-bold text-lg">{data.info.name}</h2>
-        <h3 className="font-bold text-md">
-          {"⭐" + data.info.avgRatingString + " " + data.info.sla.slaString}
-        </h3>
+        <div className="flex items-center gap-2 mt-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#ff0000" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star-icon lucide-star"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>
+            <h3 className="font-bold text-md">
+            {data.info.avgRatingString + " " + data.info.sla.slaString}
+            </h3>
+        </div>
         <p className="truncate w-full">{data.info.cuisines.join(", ")}</p>
         <p>{data.info.areaName}</p>
       </div>
